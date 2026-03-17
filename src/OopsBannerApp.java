cc1beeec26d98ee31508cde762ecee328b782c14
Anish Kumar <anishkumarprasad7070@gmail.com>
2026-03-17 15:54:14 +0530

    Refactor banner to use String array and enhanced for-loop for code reusability

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,24 +1,17 @@
 public class OopsBannerApp {
     public static void main(String[] args) throws Exception {
-       // Line 1
-        System.out.println(String.join("", "  *****   ", "  *****   ", " ******   ", " ****** "));
+        String[] bannerLines = {
+            String.join("", "  *****   ", "  *****   ", " ******   ", " ****** "),
+            String.join("", " *     *  ", " *     *  ", " *     *  ", " *      "),
+            String.join("", " *     *  ", " *     *  ", " *     *  ", " *      "),
+            String.join("", " *     *  ", " *     *  ", " ******   ", " ****** "),
+            String.join("", " *     *  ", " *     *  ", " *        ", "      * "),
+            String.join("", " *     *  ", " *     *  ", " *        ", "      * "),
+            String.join("", "  *****   ", "  *****   ", " *        ", " ****** ")
+        };
 
-        // Line 2
-        System.out.println(String.join("", " *     *  ", " *     *  ", " *     *  ", " *      "));
-
-        // Line 3
-        System.out.println(String.join("", " *     *  ", " *     *  ", " *     *  ", " *      "));
-
-        // Line 4
-        System.out.println(String.join("", " *     *  ", " *     *  ", " ******   ", " ****** "));
-
-        // Line 5
-        System.out.println(String.join("", " *     *  ", " *     *  ", " *        ", "      * "));
-
-        // Line 6
-        System.out.println(String.join("", " *     *  ", " *     *  ", " *        ", "      * "));
-
-        // Line 7
-        System.out.println(String.join("", "  *****   ", "  *****   ", " *        ", " ****** "));
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
