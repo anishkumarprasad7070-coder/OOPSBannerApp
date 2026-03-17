7a7e4334516e2c5067bfb880b32e8e330e8c008c
Anish Kumar <anishkumarprasad7070@gmail.com>
2026-03-17 15:47:42 +0530

    Refactor to use String.join() instead of string concatenation for memory efficiency

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,24 +1,24 @@
 public class OopsBannerApp {
     public static void main(String[] args) throws Exception {
        // Line 1
-        System.out.println("  *****   " + "  *****   " + " ******   " + " ****** ");
+        System.out.println(String.join("", "  *****   ", "  *****   ", " ******   ", " ****** "));
 
         // Line 2
-        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *      ");
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *     *  ", " *      "));
 
         // Line 3
-        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *      ");
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *     *  ", " *      "));
 
         // Line 4
-        System.out.println(" *     *  " + " *     *  " + " ******   " + " ****** ");
+        System.out.println(String.join("", " *     *  ", " *     *  ", " ******   ", " ****** "));
 
         // Line 5
-        System.out.println(" *     *  " + " *     *  " + " *        " + "      * ");
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *        ", "      * "));
 
         // Line 6
-        System.out.println(" *     *  " + " *     *  " + " *        " + "      * ");
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *        ", "      * "));
 
         // Line 7
-        System.out.println("  *****   " + "  *****   " + " *        " + " ****** ");
+        System.out.println(String.join("", "  *****   ", "  *****   ", " *        ", " ****** "));
     }
 }
